9c7a6a6e58d3338fff90cc5ae2e6b209ea9a6719
aharabiosciences <jacoblync@gmail.com>
2021-02-06 14:49:30 -0500

    adding auth to our serverless api

--- create.js
+++ create.js
@@ -8,13 +8,14 @@ export const main = handler(async(event, context) => {
         TableName: process.env.tableName,
         Item: {
             // The attributes of the item to be created
-            userId: "123", // The id of the author
+            userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
             noteId: uuid.v1(), // A unique uuid
             content: data.content, // Parsed from request body
             attachment: data.attachment, // Parsed from request body
             createdAt: Date.now(), // Current Unix timestamp
         },
     };
+    console.log(params);
 
-    await dynamoDb.put(params);
+    await dynamoDb.put(params).then(console.log("woof")).catch(err => console.log(err));
 });
