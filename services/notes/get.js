99771e68bb2f9bb48ee3f3b931056055746fdd55
aharabiosciences <jacoblync@gmail.com>
2021-02-07 15:51:11 -0500

    Adding a timeout

--- services/notes/get.js
+++ services/notes/get.js
@@ -1,3 +1,6 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
 export const main = handler(async (event, context) => {
     const params = {
       TableName: process.env.tableName,
